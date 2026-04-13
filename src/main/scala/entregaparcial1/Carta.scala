dc8543f67b685de30eca5004822cc9f24f6e5c5b
nicolasparram-i <nicolas.parra.m@ug.uchile.cl>
2026-04-13 18:27:56 -0400

    feat: modelado base de entidades

---
+++ src/main/scala/entregaparcial1/Carta.scala
@@ -0,0 +1,6 @@
+package cl.uchile.dcc
+package entregaparcial1
+
+class Carta(val rango: Rango, val pinta: Pinta) {
+
+}
---
+++ src/main/scala/entregaparcial1/Pinta.scala
@@ -0,0 +1,11 @@
+package cl.uchile.dcc
+package entregaparcial1
+
+trait Pinta {
+
+  object Corazon extends Pinta
+  object Pica extends Pinta
+  object Diamante extends Pinta
+  object Trebol extends Pinta
+
+}
